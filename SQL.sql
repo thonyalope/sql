2a0afdfe2f7c268fbf21436dc484e85d89cef873
thonyalope <thonyalope@gmail.com>
2023-11-28 18:08:29 -0600

    Add initial SQL queries for EmployeeDemographics and EmployeeSalary tables

---
+++ SQL.sql
@@ -0,0 +1,13 @@
+--CREATE TABLE EmployeeDemographics
+--(EmployeeID int,
+--FirstName varchar(50),
+--LastName varchar(50),
+--Age int,
+--Gender varchar(50)
+--)
+
+--CREATE TABLE EmployeeSalary
+--(EmployeeID int,
+--JobTitle varchar(50),
+--Salary int
+--)
