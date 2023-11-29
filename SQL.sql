796fb970de8a8032b7919bd45cc378f909bb5029
thonyalope <thonyalope@gmail.com>
2023-11-28 18:26:38 -0600

    Add values to EmployeeDemographics and EmployeeSalary tables

--- SQL.sql
+++ SQL.sql
@@ -11,3 +11,25 @@
 --JobTitle varchar(50),
 --Salary int
 --)
+
+--INSERT INTO EmployeeDemographics VALUES
+--(1001, 'Anthony', 'Vargas', 27, 'Male'),
+--(1002, 'Manuel', 'Vargas', 52, 'Male'),
+--(1003, 'Norma', 'Alvarado', 50, 'Female'),
+--(1004, 'Ninoska', 'Vargas', 24, 'Female'),
+--(1005, 'Josafat', 'Vargas', 25, 'Male'),
+--(1006, 'Victor', 'Vargas', 54, 'Male'),
+--(1007, 'Alejandro', 'Rivera', 29, 'Male'),
+--(1008, 'Anthony', 'Castro', 27, 'Male'),
+--(1009, 'Marco', 'Chinchilla', 28, 'Male')
+
+--INSERT INTO EmployeeSalary VALUES
+--(1001, 'Salesman', 45000),
+--(1002, 'Receptionist', 36000),
+--(1003, 'Salesman', 63000),
+--(1004, 'Accountant', 47000),
+--(1005, 'HR', 50000),
+--(1006, 'Regional Manager', 75000),
+--(1007, 'Supplier Relations', 41000),
+--(1008, 'Salesman', 45800),
+--(1009, 'Accountant', 42000)
