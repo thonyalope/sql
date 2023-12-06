3484f671ffcb6f052804355dc20b35e9e0cc0d9c
thonyalope <thonyalope@gmail.com>
2023-12-06 05:27:22 -0600

    Determine countries with highest death count per population

--- COVID_Deaths/COVID_Deaths.sql
+++ COVID_Deaths/COVID_Deaths.sql
@@ -20,15 +20,23 @@ WHERE location LIKE '%states%'
 AND continent IS NOT NULL
 ORDER BY 1,2
 
--- Total Cases vs Population
+--Total Cases vs Population
 SELECT location, date, population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
 FROM dbo.CovidDeaths
 --WHERE location LIKE '%states%'
 ORDER BY 1,2
 
--- Countries with Highest Infection Rate compared to Population
-SELECT location, population, MAX(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 AS PercentPopulationInfected
+--Countries with Highest Infection Rate compared to Population
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, Max((total_cases/population))*100 AS PercentPopulationInfected
 FROM dbo.CovidDeaths
 --Where location like '%states%'
 GROUP BY location, population
-oRDER BY PercentPopulationInfected DESC
+ORDER BY PercentPopulationInfected DESC
+
+--Countries with Highest Death Count per Population
+SELECT location, MAX(cast(total_deaths AS INT)) AS TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY TotalDeathCount DESC
