74089a55c099547353c595767b91c98fed84a8e1
thonyalope <thonyalope@gmail.com>
2023-12-06 05:14:06 -0600

    Analyze total cases vs total deaths

--- COVID_Deaths/COVID_Deaths.sql
+++ COVID_Deaths/COVID_Deaths.sql
@@ -12,3 +12,10 @@ SELECT location, date, new_cases, total_deaths, population
 FROM dbo.CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2
+
+--Total Cases vs Total Deaths
+SELECT location, date, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM dbo.CovidDeaths
+WHERE location LIKE '%states%'
+AND continent IS NOT NULL
+ORDER BY 1,2
