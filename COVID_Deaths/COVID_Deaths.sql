23d8fbc949440ae6564889afee27f24fae83f73e
thonyalope <thonyalope@gmail.com>
2023-12-06 05:04:37 -0600

    Select initial data for analysis

--- COVID_Deaths/COVID_Deaths.sql
+++ COVID_Deaths/COVID_Deaths.sql
@@ -6,3 +6,9 @@ SELECT *
 FROM dbo.CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 3,4
+
+--Select Data that we are going to be starting with
+SELECT location, date, new_cases, total_deaths, population
+FROM dbo.CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2
