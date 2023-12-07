f6bece23c4801821038e76528504811229756327
thonyalope <thonyalope@gmail.com>
2023-12-07 02:55:40 -0600

    Standardize date format in dataset

--- NashvilleHousing/NashvilleHousing.sql
+++ NashvilleHousing/NashvilleHousing.sql
@@ -4,3 +4,17 @@ Cleaning Data in SQL Queries
 
 SELECT *
 FROM dbo.NashvilleHousing
+
+--Standardize Date Format
+SELECT saleDateConverted, CONVERT(Date, SaleDate)
+FROM dbo.NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SaleDate = CONVERT(Date, SaleDate)
+
+--If it doesn't Update properly
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date, SaleDate)
